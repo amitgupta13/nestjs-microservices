443daecaea26a6fe590dc56829dcfbac45d67bd5
Your Name <you@example.com>
2024-12-02 08:12:00 +0000

    nc

--- apps/reservations/src/main.ts
+++ apps/reservations/src/main.ts
@@ -8,7 +8,7 @@ import * as cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(ReservationsModule);
   app.use(cookieParser());
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.useLogger(app.get(Logger));
   const configService = app.get(ConfigService);
   await app.listen(configService.get('PORT'));
